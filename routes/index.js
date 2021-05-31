1a7b05eadfd45dd804c42a580a5a7446bf3dbf01
KodyReid <kody.reid7@gmail.com>
2021-05-31 00:32:50 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET About Me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'My Projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'My Services' });
+});
+
+/* GET Contact Me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Info' });
+});
+
+
+module.exports = router;
