843d528c4178aa5bc74109de520e1a41ae45868b
KodyReid <kody.reid7@gmail.com>
2021-05-31 12:23:58 -0400

    About Me page completed

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', homeMessage: 'Thank you for visiting my page!' });
 });
 
 /* GET About Me page. */
